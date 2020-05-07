ecf28e66c10b43f740482d88ad4057e709b3f441
Matej Kerekrety <matej.kerekrety@gmail.com>
2020-05-07 16:01:56 +0100

    change env_vars name

--- main.py
+++ main.py
@@ -15,22 +15,22 @@ from diglett import (
 
 # TODO: pr_number can be parsed from vars['GITHUB_REF'] = 'refs/pull/3/merge'
 def main():
-    vars = os.environ
+    env_vars = os.environ
     required_vars = ['INPUT_USERNAME', 'INPUT_PR_NUMBER', 'INPUT_TOKEN']
 
-    if all(v in required_vars and vars[v] for v in vars):
+    if all(v in required_vars and env_vars[v] for v in env_vars):
         # All parameters are required
-        logging.error(f'Some of the required parameters is missing: {vars}')
-        print(f"::set-output name=OutputMessage:: Some of the required parameters is missing: {vars}")
+        logging.error(f'Some of the required parameters is missing: {env_vars}')
+        print(f"::set-output name=OutputMessage:: Some of the required parameters is missing: {env_vars}")
         sys.exit(1)
 
-    owner = vars['GITHUB_REPOSITORY_OWNER']
-    repo_name = vars['GITHUB_REPOSITORY'].split('/', 1)[1]
-    max_commits = vars.get('INPUT_MAX_COMMITS', 100)
+    owner = env_vars['GITHUB_REPOSITORY_OWNER']
+    repo_name = env_vars['GITHUB_REPOSITORY'].split('/', 1)[1]
+    max_commits = env_vars.get('INPUT_MAX_COMMITS', 100)
     max_days = 50
-    username = vars['INPUT_USERNAME']
-    token = vars['INPUT_TOKEN']
-    pr_number = vars['INPUT_PR_NUMBER']
+    username = env_vars['INPUT_USERNAME']
+    token = env_vars['INPUT_TOKEN']
+    pr_number = env_vars['INPUT_PR_NUMBER']
 
     session = make_session(username, token)
 
