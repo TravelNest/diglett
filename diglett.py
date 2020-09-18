255cdd534b1053272e2b539d87103b4dec95fd74
Matej Kerekrety <matej.kerekrety@gmail.com>
2020-09-18 15:23:44 +0100

    repo stats rebug vol5

--- diglett.py
+++ diglett.py
@@ -65,7 +65,7 @@ def total_count_commits_from_last_modified(repo_stats, last_modified=None):
 
     total = sum([s.get("total", 0) for s in repo_stats if s.get('week', 0) >= start_of_week])
 
-    return total
+    return (total, repo_stats, start_of_week)
 
 
 def get_number_of_pages_repo_list(owner, s, per_page=None, page=None):
